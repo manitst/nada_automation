f1989c275f87a2a57f1caea50da6a5419972bb21
manitst <manikandan.k@teknotrait.com>
2025-04-23 19:25:58 +0530

    more modification on contact page

--- tests/Nada/Pages/contactPage.js
+++ tests/Nada/Pages/contactPage.js
@@ -17,7 +17,18 @@ export class ContactPage{
         this.errorMessageFirstName = this.page.getByText('First name required')
         this.errorMessageLastName = this.page.getByText('Last name required');   
         this.errorMessageEmail = this.page.getByText('Email required');
-        this.errorMessagePhoneNumber = this.page.getByText('Phone number required');    
+        this.errorMessagePhoneNumber = this.page.getByText('Phone number required');
+        this.homeSharePgmbtn = this.page.getByRole('radio', { name: 'No' }) 
+        this.contactInfoMenu = this.page.getByRole('link', { name: 'Contact Info' })
+        this.contactInfoMenuNumber = this.page.locator('//*[@id="__next"]/div[1]/div[2]/aside/div[2]/a[1]/div/div');
+        this.stepNumber = this.page.locator('span:has-text("Step 1")');
+        this.NMLSConsumerAccesslink = this.page.locator('text=NMLS Consumer Access');
+        this.privacyLink = this.page.getByRole('link', { name: 'Privacy', exact: true });
+        this.termsLink = this.page.getByRole('link', { name: 'Terms', exact: true });
+        this.licensingLink = this.page.getByRole('link', { name: 'Licensing', exact: true });
+        this.logoutlink = this.page.getByRole('link', { name: 'Log Out' })
+        this.giveUsACallNumber = this.page.getByRole('link', { name: '(833) 463-' });
+        this.emailUsLink = this.page.getByRole('link', { name: 'processing@nada.co' });  
             }
   
     async navigateToHomePage() {
@@ -79,6 +90,55 @@ export class ContactPage{
         await this.errorMessagePhoneNumber.waitFor();
         return await this.errorMessagePhoneNumber.innerText();
     }
+    async clickHomeSharePgmbtn() {
+        await this.homeSharePgmbtn.click();
+    }
+    async verifyUserDirectedToPropertyDetailsPage() {
+        await this.page.waitForTimeout(5000); // Wait for 2 seconds to ensure the page has loaded
+        return await this.page.url();
+    }
+    async verifyMenuIsHighlighted() {
+        await this.contactInfoMenu.waitFor();
+        return this.contactInfoMenu;
+    }
+    async getContactInfoMenuNumber() {
+        await this.contactInfoMenuNumber.waitFor();
+        return await this.contactInfoMenuNumber.innerText();
+    }
+    async getStepNumber() {
+        await this.stepNumber.waitFor();
+        return await this.stepNumber.innerText();
+    }
+    async verifyNMLSConsumerAccesslink() {
+        await this.NMLSConsumerAccesslink.waitFor();
+        return this.NMLSConsumerAccesslink;
+    }
+    async verifyPrivacyLink() {
+        await this.privacyLink.waitFor();
+        return this.privacyLink;
+    }
+    async verifyTermsLink() {
+        await this.termsLink.waitFor();
+        return this.termsLink;
+    }
+    async verifyLicensingLink() {
+        await this.licensingLink.waitFor();
+        return this.licensingLink;
+    }
+    async verifyLogoutlink() {
+        await this.logoutlink.waitFor();
+        return this.logoutlink;
+    }
+    async verifyGiveUsACallNumberIsvisible() {
+        await this.giveUsACallNumber.waitFor();
+        return this.giveUsACallNumber;
+    }
+    async verifyEmailUsLinkIsVisible() {
+        await this.emailUsLink.waitFor();
+        return this.emailUsLink;
+    }
 
-      
+
+
+           
 }
--- tests/Nada/nada-tests/contactPage.spec.js
+++ tests/Nada/nada-tests/contactPage.spec.js
@@ -1,6 +1,7 @@
 import { test, expect, chromium } from "@playwright/test";
 import { HomePage } from "../pages/homePage.js";
 import { ContactPage } from "../pages/contactPage.js";
+import { generateUniqueEmail } from "../Utils/commonUtils.js";
 
 test.beforeEach(async ({ page }) => {
     
@@ -48,7 +49,7 @@ test('@regression @TC-NADA-22 verify placeholder text of all the text fields in 
     expect(phoneNumberPlaceHolderText).toBe('Enter mobile number');
 });
 
-test('@regression @TC-NADA-22-1 verify the error messages when user does not enters any data', async ({ page }) => {
+test('@regression @TC-NADA-22-1 @TC-NADA-26 verify the error messages when user does not enters any data', async ({ page }) => {
 
     const homepage = new HomePage(page);
     const contactpage = new ContactPage(page)
@@ -73,3 +74,89 @@ test('@regression @TC-NADA-22-1 verify the error messages when user does not ent
     console.log('errorMessagePhoneNumber-', errorMessagePhoneNumber)
     expect(errorMessagePhoneNumber).toBe('Phone number required');
 });
+test('@regression @TC-NADA-23 verify when the user enter valid data then user can proceed to next page ', async ({ page }) => {
+
+    const homepage = new HomePage(page);
+    const contactpage = new ContactPage(page)
+    
+    await homepage.navigateToHomePage()
+    await contactpage.clickApplyNowButton()
+    await contactpage.enterFirstname('Test')
+    await contactpage.enterLastname('Automation')
+    await contactpage.enterEmail(generateUniqueEmail())
+    await contactpage.enterPhonenumber('1234567890')
+    await contactpage.clickHomeSharePgmbtn()
+    await contactpage.clickNextButton()
+    const currentPage = await contactpage.verifyUserDirectedToPropertyDetailsPage();
+    console.log('currentPage-', currentPage);
+    expect(currentPage).toBe('https://hitch-hei.onrender.com/apply/home-value'); // Update the expected URL as needed
+});
+test('@regression @TC-NADA-24 verify whether the correct menu is highlighted', async ({ page }) => { 
+    
+        const homepage = new HomePage(page);
+        const contactpage = new ContactPage(page)
+        
+        await homepage.navigateToHomePage()
+        await contactpage.clickApplyNowButton()
+        const isHighlighted = await contactpage.contactInfoMenu.evaluate((element) => {
+            return window.getComputedStyle(element).borderColor === 'rgb(0, 198, 94)'; // Check if the background color is white
+        }); 
+        console.log(isHighlighted);
+        if(isHighlighted === true)
+        {
+            console.log('Contact Info menu is highlighted in green color');
+        }
+        else
+        {
+            console.log('Contact Info menu is not highlighted in green color');
+        }
+    
+    });
+    test('@regression @TC-NADA-25 verify the contact info menu number', async ({ page }) => {
+        
+            const homepage = new HomePage(page);
+            const contactpage = new ContactPage(page)
+            
+            await homepage.navigateToHomePage()
+            await contactpage.clickApplyNowButton()
+            const contactInfoMenuNumber = await contactpage.contactInfoMenuNumber.innerText();
+            console.log('contactInfoMenuNumber-', contactInfoMenuNumber);
+            expect(contactInfoMenuNumber).toBe('1'); 
+            const stepNumber = await contactpage.stepNumber.innerText();
+            console.log('stepNumber-', stepNumber);
+            expect(stepNumber).toContain('1'); 
+            const stepNumberExtracted = stepNumber.match(/\d+/)?.[0];
+            expect(contactInfoMenuNumber).toBe(stepNumberExtracted);
+    });
+    test('@regression @TC-NADA-27 verify all the links are available in the footer of the page', async ({ page }) => {
+        
+            const homepage = new HomePage(page);
+            const contactpage = new ContactPage(page)
+            await homepage.navigateToHomePage()
+            await contactpage.clickApplyNowButton()
+            expect(await contactpage.NMLSConsumerAccesslink.isVisible());
+            expect(await contactpage.privacyLink.isVisible());
+            expect(await contactpage.termsLink.isVisible());
+            expect(await contactpage.licensingLink.isVisible());
+            expect(await contactpage.logoutlink.isVisible());        
+            
+    }); 
+    test('@regression @TC-NADA-28 verify contact phone number is visible in header ', async ({ page }) => {
+            
+                const homepage = new HomePage(page);
+                const contactpage = new ContactPage(page)
+                await homepage.navigateToHomePage()
+                await contactpage.clickApplyNowButton()
+                expect(await contactpage.verifyGiveUsACallNumberIsvisible()).toBeVisible();
+                console.log('Give us a call number is visible in header');
+
+    });
+    test('@regression @TC-NADA-29 verify email option is visible in header ', async ({ page }) => {
+            
+                const homepage = new HomePage(page);
+                const contactpage = new ContactPage(page)
+                await homepage.navigateToHomePage()
+                await contactpage.clickApplyNowButton()
+                expect(await contactpage.verifyEmailUsLinkIsVisible()).toBeVisible();
+                console.log('Email us link is visible in header'); 
+    });
